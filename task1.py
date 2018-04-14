799ff73bc6b9666cd5c874762662423d07099bd8
Tijana <tijana.sekaric@udg.edu.me>
2018-04-14 17:01:41 +0200

    Zadatak1

--- task1.py
+++ task1.py
@@ -12,3 +12,32 @@
 * Use main() function to test your solution.
 ===================================================
 """
+
+import math
+
+def rect_2_polar(a,b):
+
+    z = math.sqrt(a**2 + b**2)
+
+    if a > 0:
+        angle = math.atan(b/a)
+
+    elif a < 0:
+        angle = 3.14 + math.atan(b/a)
+
+    else:
+        if b > 0:
+            angle = 3.14/2
+
+        else:
+            angle = -(3.14/2)
+
+    angle = angle * (180 / 3.14)
+
+    return "("+ str(angle)+","+str(angle)+")"
+
+def main():
+
+     print(rect_2_polar(6,2))
+
+main()
